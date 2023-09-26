505ed6c2a3ffd2525eb316538ab7e95951aee418
Orla08 <91040882+Orla08@users.noreply.github.com>
2023-09-26 16:24:52 -0500

    Migracion

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import { Text, StyleSheet, View, SafeAreaView, Icon } from 'react-native'
-import Login from './src/vistas/Login'
+import { Text, View, SafeAreaView, Icon } from 'react-native'
+import Login2 from './src/vistas/Login2'
+import Registro from './src/vistas/Registro.js'
 
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons' //Instancia Luego name y dentro del name como se llama
+import Home from './src/vistas/Home'
+import { Navegacion } from './src/vistas/navegacion/Navegacion'
 
 
 /* <Icons name='chevron-left' style={{color:'black', fontSize:30}}/> */
 function App() {
     return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Login/>
-        </View>
-      </SafeAreaView>
+          <Navegacion/>
     )
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center'
-  }
-})
+
 
 export default App
